1b8343d5be21fb2887cb78b7814093020e6a1011
i-am-gabi <gabicavalcantesilva@gmail.com>
2015-01-27 10:35:01 -0300

    conta herença

---
+++ poo/conta_especial.py
@@ -0,0 +1,10 @@
+from conta_corrente import Conta
+
+class ContaEspecial(Conta):
+    def __init__(self, clientes, numero, saldo = 0, limite = 0):
+	Conta.__init__(self, clientes, numero, saldo)
+	self.limite = limite
+    def saque(self, valor):
+	if self.saldo + self.limite >= valor:
+	    self.saldo -= valor
+	    self.operacoes.append(["SAQUE", valor])
